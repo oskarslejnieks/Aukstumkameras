ab8b7cd9c282f9644c02a4bd92b643d04b51b99f
oskarslejnieks <73002358+oskarslejnieks@users.noreply.github.com>
2021-10-03 21:04:21 +0300

    initialcommit

---
+++ Mājaslapa Edgaram/script.js
@@ -0,0 +1,54 @@
+ 
+ // Pieprasīt cenu
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal)
+  });
+});
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach(modal => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
+  });
+})
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+/////////////
+
+
+// sticky header
+
+// var stickyOffset = $('.sticky').offset().top;
+
+// $(window).scroll(function(){
+//   var sticky = $('.sticky'),
+//       scroll = $(window).scrollTop();
+
+//   if (scroll >= stickyOffset) sticky.addClass('fixed');
+//   else sticky.removeClass('fixed');
+// });
